fc3eb821d4819ab121c180fbf24d6d56ded65735
Michael Barker <michael.barker@lmax.com>
2015-02-17 10:30:56 +1300

    Add biFold and recast to prevent the construction of new instances when not required.

--- src/main/java/uk/co/bhyland/validationworkshop/Validation.java
+++ src/main/java/uk/co/bhyland/validationworkshop/Validation.java
@@ -11,9 +11,9 @@ import java.util.stream.Collectors;
 
 /**
  * The Validation class represents a disjoint union (aka sum type) of either
- *      a (non-empty) list of error values of type E, or
- *      a success value of type T
- *
+ * a (non-empty) list of error values of type E, or
+ * a success value of type T
+ * <p>
  * The internal implementation should not be observable to client code except via the Validation public api.
  */
 public abstract class Validation<E, T> {
@@ -22,49 +22,46 @@ public abstract class Validation<E, T> {
     Implement the following to make the tests pass.
     Feel free to add to or change anything about the implementation, so long as the tests still pass and the intent of the class remains.
     */
-    
-    private Validation() {}
 
-    private static class Success<E, T> extends Validation<E, T>
-    {
+    private Validation() {
+    }
+
+    private static class Success<E, T> extends Validation<E, T> {
         private final T value;
 
-        public Success(T value)
-        {
+        public Success(T value) {
             this.value = value;
-        }
-        
-        @Override
-        public <U> U fold(Function<List<E>, U> ifFailure, Function<T, U> ifSuccess)
-        {
-            return ifSuccess.apply(value);
         }
 
         @Override
-        public String toString()
-        {
+        public <U> U biFold(BiFunction<Failure<E, T>, List<E>, U> ifFailure, BiFunction<Success<E, T>, T, U> ifSuccess) {
+            return ifSuccess.apply(this, value);
+        }
+
+        @Override
+        public String toString() {
             return "Success [value=" + value + "]";
         }
     }
-    
-    private static class Failure<E, T> extends Validation<E, T>
-    {
+
+    private static class Failure<E, T> extends Validation<E, T> {
         private final List<E> failures;
 
-        public Failure(List<E> failures)
-        {
+        public Failure(List<E> failures) {
             this.failures = failures;
-        }
-        
-        @Override
-        public <U> U fold(Function<List<E>, U> ifFailure, Function<T, U> ifSuccess)
-        {
-            return ifFailure.apply(failures);
         }
 
         @Override
-        public String toString()
-        {
+        public <U> U biFold(BiFunction<Failure<E, T>, List<E>, U> ifFailure, BiFunction<Success<E, T>, T, U> ifSuccess) {
+            return ifFailure.apply(this, failures);
+        }
+
+        private <U> Validation<E, U> recast() {
+            return (Validation<E, U>) this;
+        }
+
+        @Override
+        public String toString() {
             return "Failure [failures=" + failures + "]";
         }
     }
@@ -83,34 +80,30 @@ public abstract class Validation<E, T> {
     public static <E, T> Validation<E, T> failure(final E value, final E... values) {
         return new Failure<>(makeNonEmptyList(value, values));
     }
-    
+
     private static <E, T> Validation<E, T> failure(List<E> errors) {
         return new Failure<>(errors);
     }
 
-    private <U> Validation<E, U> makeFailure(List<E> errors)
-    {
+    private <U> Validation<E, U> makeFailure(List<E> errors) {
         return Validation.<E, U>failure(errors);
     }
 
-    private <U> Validation<E, U> makeSuccess(U value)
-    {
+    private <U> Validation<E, U> makeSuccess(U value) {
         return success(value);
     }
-    
+
     /**
      * Convenience to test what this Validation represents while ignoring its contained value(s).
      */
-    public final boolean isSuccess()
-    {
+    public final boolean isSuccess() {
         return fold(es -> false, s -> true);
     }
 
     /**
      * Convenience to test what this Validation represents while ignoring its contained value(s).
      */
-    public final boolean isFailure()
-    {
+    public final boolean isFailure() {
         return !isSuccess();
     }
 
@@ -124,32 +117,13 @@ public abstract class Validation<E, T> {
     }
 
     // You may find this utility function useful
-    protected static <A> A getHeadOfList(final List<A> nonEmptyList) {
-        return nonEmptyList.get(0);
-    }
-
-    // You may find this utility function useful
-    protected static <A> A[] getTailOfList(final List<A> nonEmptyList)
-    {
-        @SuppressWarnings("unchecked")
-        final A[] tail = (A[]) new Object[nonEmptyList.size() - 1];
-        for (int i = 1; i < nonEmptyList.size(); i++)
-        {
-            tail[i - 1] = nonEmptyList.get(i);
-        }
-        return tail;
-    }
-
-    // You may find this utility function useful
     @SafeVarargs
-    protected static <A> List<A> combineLists(final List<A> first, final List<A> second, final List<A>... rest)
-    {
+    protected static <A> List<A> combineLists(final List<A> first, final List<A> second, final List<A>... rest) {
         final List<A> combined = new ArrayList<>();
         combined.addAll(first);
         combined.addAll(second);
 
-        for (List<A> as : rest)
-        {
+        for (List<A> as : rest) {
             combined.addAll(as);
         }
 
@@ -160,8 +134,7 @@ public abstract class Validation<E, T> {
      * Return the success value of this Validation, if it represents a success; otherwise, return the supplied default value.
      * The given supplier should only be invoked if its return value is required.
      */
-    public final T getOrElse(final Supplier<T> defaultValue)
-    {
+    public final T getOrElse(final Supplier<T> defaultValue) {
         return fold(
             es -> defaultValue.get(),
             s -> s
@@ -172,8 +145,7 @@ public abstract class Validation<E, T> {
      * Return this Validation, if it represents a success; otherwise, return the supplied default Validation.
      * The given supplier should only be invoked if its return value is required.
      */
-    public final Validation<E, T> orElse(Supplier<Validation<E, T>> defaultValue)
-    {
+    public final Validation<E, T> orElse(Supplier<Validation<E, T>> defaultValue) {
         return fold(
             es -> defaultValue.get(),
             s -> this
@@ -183,34 +155,43 @@ public abstract class Validation<E, T> {
     /**
      * Run exactly one of the two provided functions, depending on what this Validation represents.
      * Return the result of the selected function.
-     *
+     * <p>
      * Hint for later tasks:
-     *      Many things can be implemented in terms of fold.
-     *      What procedural code structure is represented by fold?
+     * Many things can be implemented in terms of fold.
+     * What procedural code structure is represented by fold?
      */
 
-    public abstract <U> U fold(final Function<List<E>, U> ifFailure, final Function<T, U> ifSuccess);
+    public <U> U fold(final Function<List<E>, U> ifFailure, final Function<T, U> ifSuccess) {
+        return this.<U>biFold((v, es) -> ifFailure.apply(es), (v, s) -> ifSuccess.apply(s));
+    }
+
+    public void consume(final Consumer<List<E>> ifFailure, final Consumer<T> ifSuccess) {
+        fold(
+            e -> { ifFailure.accept(e); return null; },
+            t -> { ifSuccess.accept(t); return null; }
+        );
+    }
+
+    public abstract <U> U biFold(final BiFunction<Failure<E, T>, List<E>, U> ifFailure,
+                                 final BiFunction<Success<E, T>, T, U> ifSuccess);
 
     @SafeVarargs
     public static <E> void ifFailure(
-        final Consumer<List<E>> ifFailure, 
+        final Consumer<List<E>> ifFailure,
         Validation<E, ?> validation,
-        Validation<E, ?>... moreValidations)
-    {
+        Validation<E, ?>... moreValidations) {
         validation.ifFailure(ifFailure);
-        
-        for (Validation<E, ?> v : moreValidations)
-        {
+
+        for (Validation<E, ?> v : moreValidations) {
             v.ifFailure(ifFailure);
         }
     }
-    
+
     @SafeVarargs
     public static <E> List<E> reduceErrors(
-        List<E> allErrors, 
+        List<E> allErrors,
         Validation<E, ?> validation,
-        Validation<E, ?>... moreValidations)
-    {
+        Validation<E, ?>... moreValidations) {
         ifFailure((es) -> allErrors.addAll(es), validation, moreValidations);
         return allErrors;
     }
@@ -218,27 +199,31 @@ public abstract class Validation<E, T> {
     /**
      * Convenience to perform an effect using the success value if this Validation represents a success.
      * The consumer should not be invoked if this Validation represents a failure.
-     *
+     * <p>
      * Not tested: implementing this is optional.
      */
-    public final void ifSuccess(final Consumer<T> ifSuccess)
-    {
+    public final void ifSuccess(final Consumer<T> ifSuccess) {
         fold(
             es -> null,
-            s -> { ifSuccess.accept(s); return null; }
+            s -> {
+                ifSuccess.accept(s);
+                return null;
+            }
         );
     }
 
     /**
      * Convenience to perform an effect using the failure values if this Validation represents a failure.
      * The consumer should not be invoked if this Validation represents a success.
-     *
+     * <p>
      * Not tested: implementing this is optional.
      */
-    public final void ifFailure(final Consumer<List<E>> ifFailure)
-    {
+    public final void ifFailure(final Consumer<List<E>> ifFailure) {
         fold(
-            es -> { ifFailure.accept(es); return null; },
+            es -> {
+                ifFailure.accept(es);
+                return null;
+            },
             s -> null
         );
     }
@@ -249,10 +234,10 @@ public abstract class Validation<E, T> {
      * The given function should only be invoked if its return value is required.
      */
     public <U> Validation<E, U> map(final Function<T, U> f) {
-        
-        return fold(
-            es -> this.<U>makeFailure(es),
-            t -> makeSuccess(f.apply(t))
+
+        return biFold(
+            (v, e) -> v.<U>recast(),
+            (v, t) -> makeSuccess(f.apply(t))
         );
     }
 
@@ -262,10 +247,10 @@ public abstract class Validation<E, T> {
      * The given function should only be invoked if its return value is required.
      */
     public <U> Validation<E, U> flatMap(final Function<T, Validation<E, U>> f) {
-        
-        return fold(
-            es -> this.<U>makeFailure(es),
-            t -> f.apply(t)
+
+        return biFold(
+            (v, e) -> v.<U>recast(),
+            (v, t) -> f.apply(t)
         );
     }
 
@@ -283,7 +268,7 @@ public abstract class Validation<E, T> {
      * Unfortunately, List.flatMap() doesn't actually exist, although Stream.flatMap() does.
      * You won't miss much if you skip this, but it is tested and is used in some of the example code.
      */
-    public static <E, A, B> List<Validation<E, B>> flatMapInputs(final List<Validation<E, A>> inputs, final Function<A, Validation<E, B>> f) {        
+    public static <E, A, B> List<Validation<E, B>> flatMapInputs(final List<Validation<E, A>> inputs, final Function<A, Validation<E, B>> f) {
         return inputs.stream().map((v) -> v.flatMap(f)).collect(Collectors.toList());
     }
 
@@ -291,20 +276,20 @@ public abstract class Validation<E, T> {
      * If both this Validation and the given Validation represent successes,
      * return a Validation containing the success value of this Validation transformed by the success value of the given Validation;
      * otherwise, return a Validation containing the all the available error values, and do not invoke the function that might be contained in the argument.
-     *
+     * <p>
      * Hint for later tasks:
-     *      Many things can be implemented in terms of fold and apply.
-     *      Consider making Validation.apply() final before continuing, as this should help make its structure clear.
+     * Many things can be implemented in terms of fold and apply.
+     * Consider making Validation.apply() final before continuing, as this should help make its structure clear.
      */
     public final <U> Validation<E, U> apply(final Validation<E, Function<T, U>> functionValidation) {
-        return fold(
-            errors -> functionValidation.fold(
+        return biFold(
+            (v, errors) -> functionValidation.fold(
                 errors2 -> this.<U>makeFailure(combineLists(errors, errors2)),
-                func -> this.<U>makeFailure(errors)
+                func -> v.<U>recast()
             ),
-            value -> functionValidation.fold(
-                errors -> this.<U>makeFailure(errors),
-                func -> makeSuccess(func.apply(value))
+            (v, value) -> functionValidation.biFold(
+                (v2, errors) -> v2.<U>recast(),
+                (v2, func) -> makeSuccess(func.apply(value))
             ));
     }
 
@@ -315,22 +300,18 @@ public abstract class Validation<E, T> {
      * The given list should be considered in order.
      */
     public static <E, T> Validation<E, List<T>> sequence(final List<Validation<E, T>> validations) {
-        
+
         List<E> errors = new ArrayList<>();
         List<T> values = new ArrayList<>();
-        
-        validations.forEach(validation ->
-        {
-            validation.ifSuccess(t -> values.add(t));
-            validation.ifFailure(es -> errors.addAll(es));
-        });
-        
-        if (errors.isEmpty())
-        {
+
+        validations.forEach(
+            v -> v.consume(
+                    es -> errors.addAll(es),
+                    t -> values.add(t)));
+
+        if (errors.isEmpty()) {
             return success(values);
-        }
-        else
-        {
+        } else {
             return failure(errors);
         }
     }
@@ -343,12 +324,12 @@ public abstract class Validation<E, T> {
      * Once it can be determined that the return value will represent a failure, no further invocations of the given function should occur.
      */
     public static <E, T, R> Validation<E, List<R>> traverse(final List<Validation<E, T>> validations, final Function<T, R> f) {
-        
+
         Validation<E, List<T>> sequence = sequence(validations);
-        
-        return sequence.fold(
-            es -> Validation.<E,List<R>>failure(es),
-            l -> Validation.<E,List<R>>success(l.stream().map(f).collect(Collectors.toList()))
+
+        return sequence.biFold(
+            (v, e) -> v.<List<R>>recast(),
+            (v, l) -> Validation.<E, List<R>>success(l.stream().map(f).collect(Collectors.toList()))
         );
     }
 
@@ -358,9 +339,9 @@ public abstract class Validation<E, T> {
      */
     public static <E, A, B, R> Validation<E, R> map2(final Validation<E, A> va, final Validation<E, B> vb,
                                                      final Function<A, Function<B, R>> f) {
-        
+
         return va.fold(
-            errors -> Validation.<E,R>failure(reduceErrors(new ArrayList<>(), va, vb)),
+            errors -> Validation.<E, R>failure(reduceErrors(new ArrayList<>(), va, vb)),
             a -> vb.map(f.apply(a))
         );
     }
@@ -372,7 +353,7 @@ public abstract class Validation<E, T> {
     public static <E, A, B, C, R> Validation<E, R> map3(final Validation<E, A> va, final Validation<E, B> vb, final Validation<E, C> vc,
                                                         final Function<A, Function<B, Function<C, R>>> f) {
         return va.fold(
-            errors -> Validation.<E,R>failure(reduceErrors(new ArrayList<>(), va, vb, vc)),
+            errors -> Validation.<E, R>failure(reduceErrors(new ArrayList<>(), va, vb, vc)),
             a -> map2(vb, vc, f.apply(a))
         );
     }
@@ -380,13 +361,13 @@ public abstract class Validation<E, T> {
     /**
      * If any of the given Validations represented error values, then the resulting Validation contains all available errors;
      * otherwise, it contains the result of transforming the success values of the given Validations with the given curried function.
-     *
+     * <p>
      * Not tested: implementing this is optional.
      */
     public static <E, A, B, C, D, R> Validation<E, R> map4(final Validation<E, A> va, final Validation<E, B> vb, final Validation<E, C> vc, final Validation<E, D> vd,
                                                            final Function<A, Function<B, Function<C, Function<D, R>>>> f) {
         return va.fold(
-            errors -> Validation.<E,R>failure(reduceErrors(new ArrayList<>(), va, vb, vc, vd)),
+            errors -> Validation.<E, R>failure(reduceErrors(new ArrayList<>(), va, vb, vc, vd)),
             a -> map3(vb, vc, vd, f.apply(a))
         );
     }
